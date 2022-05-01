926ff3317f75d2a2ee951b9735039c092d372ff5
Seeyou2000 <99472300+Seeyou2000@users.noreply.github.com>
2022-05-01 12:33:37 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x;
+	int i = 1;
+	int j = 0;
+	int branch = 64;
+	int temp = 64;
+
+	cin >> x;
+	while (x >= 0) {
+		while ((temp >> i) >= x) {
+			i++;
+		}
+		if ((x - 1) == 0) {
+			j++;
+			cout << j;
+			return 0;
+		}
+		branch >>= i;
+		x -= branch;
+		j++;
+		if ((x - branch) == 0) {
+			cout << j;
+			return 0;
+		}
+		branch = 64;
+	}
+	cout << j;
+}
