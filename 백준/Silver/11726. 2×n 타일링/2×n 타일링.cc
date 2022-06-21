435a8020ac3f2ed2c83e2e347e35eed21a442e19
Seeyou2000 <99472300+Seeyou2000@users.noreply.github.com>
2022-06-21 18:32:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+int N;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= N; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[N];
+	return 0;
+}
