c66c61a2e06862c4df6faf34bb06780b0d9d1d5b
Seeyou2000 <99472300+Seeyou2000@users.noreply.github.com>
2022-09-14 12:06:05 +0900

    [Bronze I] Title: Meats On The Grill, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+char grill[12][12];
+
+int main() {
+	int T;
+	int H, W;
+
+	cin >> T;
+	while (T--)
+	{
+		cin >> H >> W;
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				cin >> grill[i][j];
+			}
+		}
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = W - 1; j >= 0; j--)
+			{
+				cout << grill[i][j];
+			}
+			cout << "\n";
+		}
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				grill[i][j] = NULL;
+			}
+		}
+	}
+}
