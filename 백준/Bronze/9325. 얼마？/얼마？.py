a1b4946bd1bbc2019da918fca5c8108e96a65856
Seeyou2000 <99472300+Seeyou2000@users.noreply.github.com>
2022-11-03 20:41:49 +0900

    [Bronze III] Title: 얼마?, Time: 116 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    price = int(input())
+    for _ in range(int(input())):
+        q,p=map(int,input().split())
+        price += q*p
+    print(price)
