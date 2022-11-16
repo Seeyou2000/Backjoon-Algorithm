d7894e79d05c73b881255c9ca844c251cbc29b75
Seeyou <99472300+Seeyou2000@users.noreply.github.com>
2022-11-16 22:37:06 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.cc
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
+int main()
+{
 	int n;
 	int cnt = 0;
 	cin >> n;
-
-	while (n >= 0) {
-		if (n == 1) {
-			cout << -1;
-			return 0;
-		}
-		if (n % 5 == 0) {
+	if (n == 1 || n == 3)
+	{
+		cout << -1; 
+		return 0;
+	}
+	while (n > 0)
+	{
+		if (n % 5 == 0)
+		{
 			cnt += (n / 5);
 			cout << cnt;
 			return 0;
