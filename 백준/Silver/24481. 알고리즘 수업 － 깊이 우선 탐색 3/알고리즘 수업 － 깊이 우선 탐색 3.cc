4059ea7ad723eb2511afd6b43970d2de5285b6a6
Seeyou <99472300+Seeyou2000@users.noreply.github.com>
2022-11-26 19:49:59 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 96 ms, Memory: 11024 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+vector<int> graph[100001];
+bool visited[100001];
+int order[100001];
+int cnt = 0;
+
+void dfs(int v)
+{
+	order[v] = cnt;
+	visited[v] = true;
+	for (int i = 0; i < graph[v].size(); i++)
+	{
+		int x = graph[v][i];
+		if (!visited[x])
+		{
+			cnt++;
+			visited[x] = true;
+			dfs(x);
+		}
+	}
+	cnt--;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	memset(order, -1, sizeof(int) * 100001);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+	for (int i = 0; i < M; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++)
+		sort(graph[i].begin(), graph[i].end());
+
+	dfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << order[i] << "\n";
+
+	return 0;
+}
