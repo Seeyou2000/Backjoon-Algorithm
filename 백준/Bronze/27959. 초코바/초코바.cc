a44049988966545e3a5dca9963660822a05224a6
Seeyou <99472300+Seeyou2000@users.noreply.github.com>
2023-06-08 15:49:15 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    string res = (100 * N >= M) ? "Yes" : "No";
+
+    cout << res;
+}
